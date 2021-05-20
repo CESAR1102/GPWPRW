f0a39cfaeb598f3462f1ae59d132c9f04a40032a
CESAR1102 <49628527+CESAR1102@users.noreply.github.com>
2021-05-20 01:03:45 -0500

    agregar backend

---
+++ PRY2020237.Service/IMailService.cs
@@ -0,0 +1,14 @@
+﻿using PRY2020237.Entity.Settings;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRY2020237.Service
+{
+    public interface IMailService
+    {
+        void SendEmailRecoveryPassword(MailParameter mail);
+        void SendEmail(MailRequest mailRequest);
+    }
+}
